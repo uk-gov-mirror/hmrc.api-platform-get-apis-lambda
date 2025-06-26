0b94116c6c2a69602cb8e77f4b26c0594f1ef414
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-06-26 07:59:01 +0100

    API-8467 - Initial dabble with Lanmba correct use

--- src/main/scala/uk/gov/hmrc/apiplatform/getapis/GetApisHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/getapis/GetApisHandler.scala
@@ -2,26 +2,43 @@ package uk.gov.hmrc.apiplatform.getapis
 
 import java.net.HttpURLConnection.HTTP_OK
 
-import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent}
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
-import software.amazon.awssdk.services.apigateway.model._
+import software.amazon.awssdk.services.apigateway.model.{GetRestApisRequest, GetRestApisResponse}
+import cats.syntax.either._
+import com.amazonaws.services.lambda.runtime.Context
+import io.circe.Json
+import io.circe.generic.semiauto._
+import io.circe.parser._
+import io.github.mkotsur.aws.handler.Lambda
+import io.github.mkotsur.aws.handler.Lambda._
+import io.github.mkotsur.aws.proxy._
+
 import uk.gov.hmrc.api_platform_manage_api.AwsApiGatewayClient.awsApiGatewayClient
 import uk.gov.hmrc.api_platform_manage_api.utils.ProxiedRequestHandler
 
 import scala.annotation.tailrec
-import scala.collection.JavaConverters._
-import scala.language.postfixOps
+import scala.jdk.CollectionConverters._
+import io.circe.Encoder
+import io.circe.Decoder
 
-class GetApisHandler(apiGatewayClient: ApiGatewayClient, limit: Int = 500) extends ProxiedRequestHandler {
-  def this() {
-    this(awsApiGatewayClient)
-  }
+object GetApisHandler {
+  case class Api(id: String, name: String)
+  implicit val apiEncoder: Encoder[Api] = deriveEncoder[Api]
 
-  override def handleInput(input: APIGatewayProxyRequestEvent): APIGatewayProxyResponseEvent = {
-    new APIGatewayProxyResponseEvent()
-      .withStatusCode(HTTP_OK)
-      .withBody(toJson(GetApisResponse(getApis(Seq.empty, None))))
-  }
+  case class GetApisResponse(restApis: Seq[Api])
+  implicit val getApisResponseEncoder: Encoder[GetApisResponse] = deriveEncoder[GetApisResponse]
+
+  implicit val requestContextAuthoriserDecoder: Decoder[RequestContextAuthorizer] = deriveDecoder[RequestContextAuthorizer]
+  implicit val requestContextDecoder: Decoder[RequestContext] = deriveDecoder[RequestContext]
+}
+
+import GetApisHandler._
+
+class GetApisHandler(apiGatewayClient: ApiGatewayClient = ApiGatewayClient.create(), limit: Int = 500) extends Lambda.ApiProxy[Unit, RequestContext, GetApisResponse] {
+  override protected def handle(input: ApiProxyRequest[Unit, RequestContext]) =
+    Right(ApiProxyResponse.success(Some(getApisResponse())))
+
+  private def getApisResponse(): GetApisResponse = GetApisResponse(getApis(Seq.empty, None))
 
   @tailrec
   private def getApis(apis: Seq[Api], position: Option[String]): Seq[Api] = {
@@ -41,6 +58,3 @@ class GetApisHandler(apiGatewayClient: ApiGatewayClient, limit: Int = 500) exten
     }
   }
 }
-
-case class GetApisResponse(restApis: Seq[Api])
-case class Api(id: String, name: String)
