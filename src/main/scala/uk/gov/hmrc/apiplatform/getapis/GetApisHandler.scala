e58caaa7ffb755d3a913e6f5829497c1bfb7a552
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-06-30 13:03:22 +0100

    API-8467 - Get tests passing except for exceptions which now respond with 500

--- src/main/scala/uk/gov/hmrc/apiplatform/getapis/GetApisHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/getapis/GetApisHandler.scala
@@ -9,23 +9,33 @@ import com.amazonaws.services.lambda.runtime.Context
 import io.circe.Json
 import io.circe.generic.semiauto._
 import io.circe.parser._
+import io.circe.Encoder
+import io.circe.Decoder
 import io.github.mkotsur.aws.handler.Lambda
 import io.github.mkotsur.aws.handler.Lambda._
 import io.github.mkotsur.aws.proxy._
+import io.github.mkotsur.aws.handler.CanEncode
 
 import uk.gov.hmrc.api_platform_manage_api.AwsApiGatewayClient.awsApiGatewayClient
 import uk.gov.hmrc.api_platform_manage_api.utils.ProxiedRequestHandler
 
 import scala.annotation.tailrec
 import scala.jdk.CollectionConverters._
+import software.amazon.awssdk.services.apigateway.model.UnauthorizedException
+import java.io.ByteArrayOutputStream
+import java.nio.charset.Charset
+import io.circe.Json
+import io.circe.syntax._, io.circe.generic.semiauto._
+import io.circe.parser._
 import io.circe.Encoder
 import io.circe.Decoder
+
 
 object GetApisHandler {
   case class Api(id: String, name: String)
   implicit val apiEncoder: Encoder[Api] = deriveEncoder[Api]
 
-  case class GetApisResponse(restApis: Seq[Api])
+  case class GetApisResponse(restApis: List[Api])
   implicit val getApisResponseEncoder: Encoder[GetApisResponse] = deriveEncoder[GetApisResponse]
 
   implicit val requestContextAuthoriserDecoder: Decoder[RequestContextAuthorizer] = deriveDecoder[RequestContextAuthorizer]
@@ -34,16 +44,18 @@ object GetApisHandler {
 
 import GetApisHandler._
 
-class GetApisHandler(apiGatewayClient: ApiGatewayClient = ApiGatewayClient.create(), limit: Int = 500) extends Lambda.ApiProxy[Unit, RequestContext, GetApisResponse] {
-  override protected def handle(input: ApiProxyRequest[Unit, RequestContext]) =
-    Right(ApiProxyResponse.success(Some(getApisResponse())))
+class GetApisHandler(apiGatewayClient: ApiGatewayClient = ApiGatewayClient.create(), limit: Int = 500) extends Lambda.ApiProxy[String, RequestContext, GetApisResponse] {
 
-  private def getApisResponse(): GetApisResponse = GetApisResponse(getApis(Seq.empty, None))
+  override protected def handle(input: ApiProxyRequest[String, RequestContext]) = {
+    Right(ApiProxyResponse.success(Some(getApisResponse())))
+  }
+
+  private def getApisResponse(): GetApisResponse = GetApisResponse(getApis(List.empty, None))
 
   @tailrec
-  private def getApis(apis: Seq[Api], position: Option[String]): Seq[Api] = {
+  private def getApis(apis: List[Api], position: Option[String]): List[Api] = {
     val response: GetRestApisResponse = apiGatewayClient.getRestApis(buildRequest(position))
-    val moreApis: Seq[Api] = response.items().asScala.map(item => Api(item.id(), item.name())).toSeq
+    val moreApis: List[Api] = response.items().asScala.toList.map(item => Api(item.id(), item.name()))
     if (response.position == null) {
       apis ++ moreApis
     } else {
--- src/test/scala/uk/gov/hmrc/apiplatform/getapis/GetApisHandlerSpec.scala
+++ src/test/scala/uk/gov/hmrc/apiplatform/getapis/GetApisHandlerSpec.scala
@@ -12,42 +12,105 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest._
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
 import software.amazon.awssdk.services.apigateway.model.{RestApi, _}
+import software.amazon.awssdk.utils.StringInputStream
+import io.github.mkotsur.aws.handler.CanDecode
+import io.circe.Json
+import io.circe.generic.auto._, io.circe.syntax._
+import io.circe.parser._
+import io.circe.Encoder
+import io.circe.Decoder
+import io.github.mkotsur.aws.handler._
+import java.io._
+import scala.util.Using
+import java.nio.charset.StandardCharsets
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler
+import scala.util.Try
+import com.amazonaws.services.lambda.runtime.Context
+import io.github.mkotsur.aws.proxy.ApiProxyRequest
+import io.github.mkotsur.aws.proxy.RequestContext
 
-class GetApisHandlerSpec extends AnyWordSpec with Matchers with MockitoSugar {
+import io.github.mkotsur.aws.proxy._
+import uk.gov.hmrc.apiplatform.getapis.GetApisHandler.Api
+import uk.gov.hmrc.apiplatform.getapis.GetApisHandler.GetApisResponse
+
+object GetApisHandlerSpec {
+  def makeCall[O: Decoder](handler: RequestStreamHandler)(context: Context): Either[io.circe.Error, ApiProxyResponse[O]] = {
+    val res = Using.Manager { use =>
+      val apiProxyRequest = ApiProxyRequest[Unit, RequestContext](path = "/", httpMethod = "GET", requestContext = RequestContext(None), body = None)
+      val requestAsJson = apiProxyRequest.asJson.toString()
+    
+      val is = use(new StringInputStream(requestAsJson))
+      val os = use(new ByteArrayOutputStream())
+
+      handler.handleRequest(is, os, context)
+
+      print("About to decode")
+      decode[ApiProxyResponse[String]](os.toString())
+    }
+    // println(res)
+    res.get.map( r => {
+      val o: Option[O] = r.body.fold[Option[O]](None)(rb => decode[O](rb).toOption)
+      ApiProxyResponse(r.statusCode, r.headers, o)
+    })
+  }
+}
+
+class GetApisHandlerSpec extends AnyWordSpec with Matchers with MockitoSugar with EitherValues with OptionValues with Inside {
 
   trait Setup {
     val mockAPIGatewayClient: ApiGatewayClient = mock[ApiGatewayClient]
     val getApisHandler = new GetApisHandler(mockAPIGatewayClient)
   }
-
+  
   "Get APIs Handler" should {
     "retrieve the APIs from the API Gateway" in new Setup {
       val apiGatewayResponse: GetRestApisResponse = GetRestApisResponse.builder().items(
         RestApi.builder().id("1").name("API 1").build(),
         RestApi.builder().id("2").name("API 2").build()
       ).build()
+
       when(mockAPIGatewayClient.getRestApis(any[GetRestApisRequest])).thenReturn(apiGatewayResponse)
 
-      val response: APIGatewayProxyResponseEvent = getApisHandler.handleInput(new APIGatewayProxyRequestEvent())
-
-      response.getStatusCode shouldEqual HTTP_OK
-      response.getBody shouldEqual """{"restApis":[{"id":"1","name":"API 1"},{"id":"2","name":"API 2"}]}"""
+      val response = GetApisHandlerSpec.makeCall[GetApisResponse](getApisHandler)(mock[Context])
+      
+      inside(response.value) {
+        case ApiProxyResponse(status, _, ob) =>
+          status shouldBe HTTP_OK
+          ob.value shouldBe GetApisResponse(
+            List(
+              Api("1", "API 1"),
+              Api("2", "API 2")
+            )
+          )
+      }
     }
 
     "retrieve the APIs in multiple requests if the number of APIs exceeds the limit per request" in new Setup {
       val apiGatewayFirstResponse: GetRestApisResponse = GetRestApisResponse.builder().items(
         RestApi.builder().id("1").name("API 1").build()
       ).position(UUID.randomUUID().toString).build()
+
       val apiGatewaySecondResponse: GetRestApisResponse = GetRestApisResponse.builder().items(
         RestApi.builder().id("2").name("API 2").build()
       ).build()
+      
       when(mockAPIGatewayClient.getRestApis(any[GetRestApisRequest]))
         .thenReturn(apiGatewayFirstResponse, apiGatewaySecondResponse)
 
-      val response: APIGatewayProxyResponseEvent = new GetApisHandler(mockAPIGatewayClient, limit = 1).handleInput(new APIGatewayProxyRequestEvent())
+      val limitedHandler = new GetApisHandler(mockAPIGatewayClient, limit = 1)
+      val response = GetApisHandlerSpec.makeCall[GetApisResponse](limitedHandler)(mock[Context])
 
-      response.getStatusCode shouldEqual HTTP_OK
-      response.getBody shouldEqual """{"restApis":[{"id":"1","name":"API 1"},{"id":"2","name":"API 2"}]}"""
+      inside(response.value) {
+        case ApiProxyResponse(status, _, ob) =>
+          status shouldEqual HTTP_OK
+          // ob.get shouldEqual """{"restApis":[{"id":"1","name":"API 1"},{"id":"2","name":"API 2"}]}"""
+          ob.value shouldBe GetApisResponse(
+            List(
+              Api("1", "API 1"),
+              Api("2", "API 2")
+            )
+          )
+      }
     }
 
     "propagate UnauthorizedException thrown by AWS SDK when retrieving APIs" in new Setup {
@@ -55,8 +118,13 @@ class GetApisHandlerSpec extends AnyWordSpec with Matchers with MockitoSugar {
       val id: String = UUID.randomUUID().toString
       when(mockAPIGatewayClient.getRestApis(any[GetRestApisRequest])).thenThrow(UnauthorizedException.builder().message(errorMessage).build())
 
-      val exception: UnauthorizedException = intercept[UnauthorizedException](getApisHandler.handleInput(new APIGatewayProxyRequestEvent()))
-      exception.getMessage shouldEqual errorMessage
+      val response = GetApisHandlerSpec.makeCall[GetApisResponse](getApisHandler)(mock[Context])
+
+      inside(response.value) {
+        case ApiProxyResponse(status, _, ob) => 
+          status shouldBe 500
+          // ob.value shouldBe errorMessage
+      }
     }
   }
 }
